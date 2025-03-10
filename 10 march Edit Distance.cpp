33be756eaa7ee62aa12fdb1dd683165c7ecc7a6f
Mohan Sharma <61463072+mini7007@users.noreply.github.com>
2025-03-10 18:45:56 +0530

    10 march done

---
+++ 10 march Edit Distance.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+        int editDistance(string& s1, string& s2) {
+            int m = s1.size(), n = s2.size();
+            vector<int> prev(n + 1), curr(n + 1);
+            iota(prev.begin(), prev.end(), 0);
+            for (int i = 1; i <= m; i++) {
+                curr[0] = i;
+                for (int j = 1; j <= n; j++)
+                    curr[j] = s1[i-1] == s2[j-1] ? prev[j-1] : 1 + min({prev[j-1], prev[j], curr[j-1]});
+                swap(prev, curr);
+            }
+            return prev[n];
+        }
+    };
